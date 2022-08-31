1d169b1a08d73390d12a66f3eb33fda2352341c3
godcclike <mega_explotion@hotmail.com>
2022-08-31 20:40:09 +0800

    change chmod

--- bestdayever.sh
+++ bestdayever.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-echo "Good morning Jonathan!!"
+name=$1
+compliment=$2
 
+user=$(whoami)
+date=$(date)
+whereami=$(pwd)
+
+echo "Good morning $name!!"
 sleep 1
-
-echo "You're looking good today"
-
+echo "You're looking good today $name!!"
 sleep 1
+echo "You are the most $compliment person on earth $name!!"
+sleep 2
 
-echo "You are the most handsome guy on earth!!"
+echo "you are currently logged in as $user and you are in the directory $whereami. Also today is: $date"
+
+
+# name=$1
+# compliment=$2
+
+# echo "Good morning $name!!"
+# sleep 1
+# echo "You're looking good today $name!!"
+# sleep 1
+# echo "You are the most $compliment person on earth $name!!"
+
+
+# read name
+
+# echo "Good morning $name!!"
+# sleep 1
+# echo "You're looking good today $name!!"
+# sleep 1
+# echo "You are the most $compliment person on earth $name!!"
