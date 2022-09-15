3c0c255968f63d1963e50e0ae524e9a0b12a18e0
godcclike <mega_explotion@hotmail.com>
2022-09-15 17:58:42 +0800

    upload bash loops

--- loops.sh
+++ loops.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
 
+# x=1
+
+# while [[ $x -le 100 ]]
+# do
+#   read -p "Pushup $x: Press enter to continue"
+#   (( x ++ ))
+#   echo "Congrats you have done $x pushups"
+# done
+
+# until [[ $order == coffee ]]
+# do
+#   echo "Would you like coffee or tea?"
+#   read order
+# done
+# echo "Excellent choice, here is your coffee"
+
+# for cups in {1..10};
+# do
+#   echo "Hey you've had $cups cups of coffee today"
+# done
+
+for x in google.com bing.com facebook.com 123.com;
+do
+  if ping -q -c 2 -W 1 $x > /dev/null;
+  then
+    echo "$x is up"
+    else
+        echo "$x is down"
+  fi
+done
