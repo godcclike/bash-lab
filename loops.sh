213e884dcd48b1e2ed4de08878650675715b26df
godcclike <mega_explotion@hotmail.com>
2022-09-15 18:07:32 +0800

    update loops bash

--- loops.sh
+++ loops.sh
@@ -21,12 +21,42 @@
 #   echo "Hey you've had $cups cups of coffee today"
 # done
 
-for x in google.com bing.com facebook.com 123.com;
+# for x in google.com bing.com facebook.com 123.com;
+# do
+#   if ping -q -c 2 -W 1 $x > /dev/null;
+#   then
+#     echo "$x is up"
+#     else
+#         echo "$x is down"
+#   fi
+# done
+
+# echo "What website do you want to check"
+# read target
+
+# while true
+# do
+#     if ping -q -c 2 -W 1 $target > /dev/null;
+#     then
+#         echo "$target is up"
+#         break
+#     else
+#         echo "$target is currently down"
+#     fi
+# sleep 1
+# done
+
+echo "Welcome to the Hollywood Tower Hotel"
+sleep 1
+echo "Going up"
+sleep 1
+
+for x in {1..17};
 do
-  if ping -q -c 2 -W 1 $x > /dev/null;
-  then
-    echo "$x is up"
-    else
-        echo "$x is down"
-  fi
+    if [[ $x == 13 ]];
+    then
+        continue
+    fi
+    echo "Floor $x"
+    sleep 1
 done
